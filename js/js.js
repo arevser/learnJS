1d9f63495a5ddf6719b4288e5d2aa699f295c5a2
arevser <arevser@gmail.com>
2018-07-15 17:16:50 +0300

    Date object

--- js/js.js
+++ js/js.js
@@ -1,3 +1,20 @@
+// function getWeekDay(date) {
+//     var days = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+//
+//     return days[date.getDay()];
+// }
+//
+// var date = new Date(2014, 0, 3); // 3 января 2014
+// console.log( getWeekDay(date) ); // 'пт'
+//
+//
+// let dateSecond = new Date(2014,0,3);
+// let options = {
+//    weekday: 'short'
+// };
+// console.log(dateSecond.toLocaleString("ru", options));
+
+
 // let btn = document.querySelector(".btn");
 //
 // function myAnimation() {
